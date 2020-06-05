6a8d2404b371276d0ea47ed43f0925b812580a5f
oludemilade <oludemiladeraji@gmail.com>
2020-06-04 18:34:08 -0700

    Initial Commit

---
+++ african-proverbs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  african-proverbs
+//
+//  Created by Oludemilade Raji on 6/4/20.
+//  Copyright © 2020 DonRajon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
